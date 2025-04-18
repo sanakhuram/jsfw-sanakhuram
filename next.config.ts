cf31ae4ebdef1bccba5bf8114fdf94b678dbd13b
Sana Khuram <sana.khuram.baig@gmail.com>
2025-04-18 17:24:31 +0200

    Add new features and Packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.noroff.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // ... other config options
 };
 
-export default nextConfig;
+module.exports = nextConfig;
