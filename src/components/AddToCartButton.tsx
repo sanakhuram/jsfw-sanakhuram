bb9c8d470baa4c65028693191e33d0b40bff5289
Sana Khuram <sana.khuram.baig@gmail.com>
2025-04-19 14:30:52 +0200

    Added add to cart Button🐞

---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { Product } from "@/types/product";
+import { useCart } from "@/context/CartContext";
+import toast from "react-hot-toast";
+
+export default function AddToCartButton({ product }: { product: Product}){
+    const { addToCart } = useCart();
+    
+    const handleAddToCart = () => {
+        addToCart(product);
+        toast.success(`${product.title} added to cart`);
+    };
+
+    return(
+        <button
+        onClick={handleAddToCart}
+        className="mt-4 bg-red-900 text-orange-200 rounded hover:bg-red-700"
+        >
+            Add to Cart
+        </button>
+    );
+}
